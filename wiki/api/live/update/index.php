68f02297f1591656afdc626c56ea700f276e4449
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-05-12 12:17:09 +0200

    Replaced JSON input with POST

--- wiki/api/live/update/index.php
+++ wiki/api/live/update/index.php
@@ -11,11 +11,8 @@ header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$json = file_get_contents('php://input');
-$json = json_decode($json);
-
-$id     = $json->id;
-$value  = $json->value;
+$id     = $_POST['id'];
+$value  = $_POST['value'];
 
 if($id == null || $value == null){
     $data = [
