7e948916894273be258df8e40bfe7b385d305a2b
JordyVanEssen <jordy.esse@gmail.com>
2020-05-28 15:28:56 +0200

    updated graph

--- wiki/assets/js/graph.js
+++ wiki/assets/js/graph.js
@@ -1,77 +1,43 @@
 let chart;
+var dataArr = [];
+var labels = [];
+var sock = new WebSocket("ws://172.19.3.116:5000");  //replace this address with the one the node.js server prints out. 
 
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
-    };
-    xhr.send();
-};
-
-getJSON('api/live/update/sensData.json',function(err, data) {
-    if (err !== null) {
-        alert('Something went wrong: ' + err);
-    }
-    var labels = data.amounts.map(function(e) {
-        return e[0];
-    });
-    var data = data.amounts.map(function(e) {
-        return e[1];
-    });
-
-    var ctx = document.getElementById('myChart').getContext('2d');
-    chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label:"Sensor waarde",
-                backgroundColor: 'rgb(129, 198, 2228)',
-                borderColor: "#48BF84",
-                fill: false,
-                data: data
+sock.onmessage = function (event) {
+    update(JSON.parse(event.data));
+}
+    
+var ctx = document.getElementById('myChart').getContext('2d');
+chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: "Sensor waarde",
+            backgroundColor: 'rgb(129, 198, 2228)',
+            borderColor: "#48BF84",
+            fill: false,
+            data: dataArr
+        }]
+    },
+    options: {
+        responsive: 'true',
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
             }]
         },
-        options: {
-            responsive: 'true',
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            },
-        }
-    });
+    }
 });
-
-function updateGraph() {
-    getJSON('api/live/update/sensData.json',function(err, data) {
-        if (err !== null) {
-            alert('Something went wrong: ' + err);
-        }
-        var labels = data.amounts.map(function(e) {
-            return e[0];
-        });
-        var data = data.amounts.map(function(e) {
-            return e[1];
-        });
-        chart.data.labels = labels;
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data = data;
-        });
-        chart.update();
-    });
+    
+function update(json){
+    if(dataArr.length > 100){
+       dataArr.shift()
+       labels.shift()
+    }
+    labels.push(json["sensor"]["timeStamp"]);
+    dataArr.push(json["sensor"]["value"]);
+    chart.update();
 }
-
-window.setInterval(function(){
-    updateGraph();
-    console.log("Update graph");
-}, 100);
