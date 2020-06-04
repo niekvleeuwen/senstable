8ec4dae29a58da9e142ad150f5e2330efe9b31ac
JordyVanEssen <jordy.esse@gmail.com>
2020-06-04 09:18:20 +0200

    updated server - multiple client connections

--- sensor_server_connection/server.js
+++ sensor_server_connection/server.js
@@ -12,28 +12,42 @@ const server = http.createServer(app);//create a server
 const WebSocket = require('ws');
 const s = new WebSocket.Server({ server });
 
+var clientDict = [];
+
+console.log("Server started.");
+
 s.on('connection', function (ws, req) {
     ws.on('message', function (message) {
         try {
-            var now = new Date();
-            message = JSON.parse(message)
-            message["sensor"]["timeStamp"] = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-            console.log(message["sensor"]);
-        } catch (err) {
-            console.error(err)
-        }
-        s.clients.forEach(function (client) { //broadcast incoming message to all clients (s.clients)
-            if (client != ws && client.readyState) { //except to the same client (ws) that sent this message
-                client.send(JSON.stringify(message));
+            message = JSON.parse(message);
+            console.log(message);
+
+            if(message.hasOwnProperty("client")){
+                if(clientDict[message["client"]["id"]] === undefined)
+                    clientDict[message["client"]["id"]] = [];
+                
+                clientDict[message["client"]["id"]].push(ws);
             }
-        });
+            else{
+                var now = new Date();
+                message["sensor"]["timeStamp"] = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+            }
+        } catch (err) {
+            console.error(err);
+        }
+        if(message.hasOwnProperty("sensor")){
+            if(!(clientDict[message["sensor"]["id"]] === undefined)){
+                clientDict[message["sensor"]["id"]].forEach(client => {
+                    client.send(JSON.stringify(message));
+                });
+            }
+        }
     });
 
     ws.on('close', function () {
         console.log("lost one client");
     });
-    //ws.send("new client connected");
     console.log("new client connected");
 });
 
-server.listen(5000);
+server.listen(5050);
